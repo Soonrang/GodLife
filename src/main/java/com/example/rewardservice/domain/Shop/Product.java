0cc2de21b3bc35139af1cbcc2f0842f119cd4a71
Soonrang <clearsbyul30@naver.com>
2024-06-25 01:33:39 +0900

    feat: product 엔터티 추가

---
+++ src/main/java/com/example/rewardservice/domain/Shop/Product.java
@@ -0,0 +1,47 @@
+package com.example.rewardservice.domain.Shop;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "product_category")
+    private String category;
+
+    //회사 계정을 따로 만들어 연결
+    @Column(name = "product_company")
+    private String company;
+
+    @Column(name = "product_name")
+    private String name;
+
+    @Column(name = "product_price")
+    private BigDecimal price;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ProductImage> productImages;
+
+    @Column(name= "product_description")
+    private String description;
+
+    @Column(name= "product_created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name="product_last_update")
+    private LocalDateTime lastUpdateDate;
+}
---
+++ src/main/java/com/example/rewardservice/domain/Shop/ProductImage.java
@@ -0,0 +1,24 @@
+package com.example.rewardservice.domain.Shop;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;  // Link to the Product entity
+
+    @Column(name = "image_url")
+    private String imageUrl;
+}
