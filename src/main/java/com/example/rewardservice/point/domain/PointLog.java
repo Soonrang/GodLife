8a16207b398f700f65e234e598f37781a81c84f8
정민지 <135775059+Soonrang@users.noreply.github.com>
2024-07-10 15:24:48 +0900

    feat: BaseEntity, PointLog 엔터티

---
+++ src/main/java/com/example/rewardservice/point/domain/PointLog.java
@@ -0,0 +1,32 @@
+package com.example.rewardservice.point.domain;
+
+import com.example.rewardservice.common.BaseEntity;
+import com.example.rewardservice.user.domain.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+public class PointLog extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false, updatable = false)
+    private User user;
+
+    @Column(name = "transaction_type")
+    private String transactionType;
+
+    @Column(name = "point_change")
+    private long pointChange;
+
+    @Column(name = "description")
+    private String description;
+}
