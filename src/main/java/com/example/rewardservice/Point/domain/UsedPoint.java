0de242433e07b9002be2f3402cc0dbc5dae3255a
정민지 <135775059+Soonrang@users.noreply.github.com>
2024-07-15 15:28:24 +0900

    feat: 출석 로직 구현

---
+++ src/main/java/com/example/rewardservice/Point/domain/UsedPoint.java
@@ -0,0 +1,23 @@
+//package com.example.rewardservice.Point.domain;
+//
+//import com.example.rewardservice.Event.domain.Event;
+//import com.example.rewardservice.user.domain.User;
+//import jakarta.persistence.DiscriminatorValue;
+//import jakarta.persistence.Entity;
+//import lombok.Builder;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@Entity
+//@DiscriminatorValue("USED")
+//public class UsedPoint extends Point{
+//
+//    @Builder
+//    public UsedPoint(Event event, User user, long pointChange) {
+//        super(event, user, pointChange);
+//    }
+//}
--- src/main/java/com/example/rewardservice/point/presentation/PointController.java
+++
@@ -1,4 +0,0 @@
-package com.example.rewardservice.point.presentation;
-
-public class PointController {
-}
