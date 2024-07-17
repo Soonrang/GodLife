5977214b51bcbb076875aef2db18611e0d6b87f8
Soonrang <clearsbyul30@naver.com>
2024-07-18 07:47:05 +0900

    feat: 상품, 카테고리 구현

---
+++ src/main/java/com/example/rewardservice/shop/application/dto/CategoryResponse.java
@@ -0,0 +1,22 @@
+package com.example.rewardservice.shop.application.dto;
+
+import com.example.rewardservice.shop.domain.Category;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
+
+@Getter
+@RequiredArgsConstructor
+public class CategoryResponse {
+
+    private final UUID id;
+    private final String name;
+
+    public static CategoryResponse of(final Category category) {
+        return new CategoryResponse(
+                category.getId(),
+                category.getName()
+        );
+    }
+}
