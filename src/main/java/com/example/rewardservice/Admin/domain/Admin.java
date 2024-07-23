1fb16b5982793368907b9d8c3a06f70ed896bb66
정민지 <135775059+Soonrang@users.noreply.github.com>
2024-07-23 15:27:03 +0900

    feat: 관리자 엔터티 및 서비스 추가

---
+++ src/main/java/com/example/rewardservice/Admin/domain/Admin.java
@@ -0,0 +1,23 @@
+package com.example.rewardservice.Admin.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private final String id;
+
+    @Column(name = "admin_email")
+    private final String adminEmail;
+
+    @Column(name = "admin_password")
+    private final String adminPassword;
+
+}
