f282c7cd4eaf78029bb045cf70857ba68ff36d65
정민지 <135775059+Soonrang@users.noreply.github.com>
2024-07-31 15:22:51 +0900

    feat: 페이지 뷰 이벤트 구현

---
+++ src/main/java/com/example/rewardservice/point/application/dto/ViewPointRequest.java
@@ -0,0 +1,9 @@
+package com.example.rewardservice.point.application.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ViewPointRequest {
+    private String pageName;
+    private long points;
+}
