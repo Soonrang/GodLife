d094a0e2f2c2f9269928a2d7d746e23546de579e
정민지 <135775059+Soonrang@users.noreply.github.com>
2024-08-02 15:27:21 +0900

    feat: 날씨 포인트 전송

---
+++ src/main/java/com/example/rewardservice/event/application/request/WeatherPointRequest.java
@@ -0,0 +1,11 @@
+package com.example.rewardservice.event.application.request;
+
+import lombok.Getter;
+
+import java.util.UUID;
+
+
+@Getter
+public class WeatherPointRequest {
+    private long points;
+}
