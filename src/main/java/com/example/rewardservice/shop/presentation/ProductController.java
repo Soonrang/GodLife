ae11f36e94efee4472336ec3dcf4571533c739b5
정민지 <135775059+Soonrang@users.noreply.github.com>
2024-08-05 15:26:11 +0900

    feat: s3이미지 전송 테스트

--- src/main/java/com/example/rewardservice/shop/presentation/ProductController.java
+++ src/main/java/com/example/rewardservice/shop/presentation/ProductController.java
@@ -4,9 +4,12 @@ import com.example.rewardservice.auth.AuthUser;
 import com.example.rewardservice.security.jwt.JwtTokenExtractor;
 import com.example.rewardservice.shop.application.response.ProductEasyInfoResponse;
 import com.example.rewardservice.shop.application.response.ProductInfoResponse;
+import com.example.rewardservice.shop.application.response.ProductPagingResponse;
 import com.example.rewardservice.shop.application.service.ProductService;
 import com.example.rewardservice.user.domain.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -34,9 +37,15 @@ public class ProductController {
         return ResponseEntity.ok(product);
     }
 
+//    @GetMapping
+//    public ResponseEntity<List<ProductInfoResponse>> getAllProducts() {
+//        List<ProductInfoResponse> products = productService.getAllProducts();
+//        return ResponseEntity.ok(products);
+//    }
+
     @GetMapping
-    public ResponseEntity<List<ProductInfoResponse>> getAllProducts() {
-        List<ProductInfoResponse> products = productService.getAllProducts();
-        return ResponseEntity.ok(products);
+    public ResponseEntity<Page<ProductPagingResponse>> getPagedProducts(Pageable pageable) {
+        Page<ProductPagingResponse> pagedProducts = productService.paging(pageable);
+        return ResponseEntity.ok(pagedProducts);
     }
 }
