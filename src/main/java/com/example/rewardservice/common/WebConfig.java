0606423417ae473d9c82b53054fa70442bd217a1
Soonrang <clearsbyul30@naver.com>
2024-08-05 21:25:52 +0900

    add: aws properties생성

--- src/main/java/com/example/rewardservice/common/WebConfig.java
+++ src/main/java/com/example/rewardservice/common/WebConfig.java
@@ -28,7 +28,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-            .allowedOrigins("http://localhost:5173") // 프론트엔드 주소
+            .allowedOrigins("https://www.pointsmilet.store", "http://localhost:5173") // 프론트엔드 주소
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowedHeaders("*")
             .allowCredentials(true);
