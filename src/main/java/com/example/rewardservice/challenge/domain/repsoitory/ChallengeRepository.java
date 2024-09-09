160c6de73b9e7f3680f7d58371e80a380d63c4f4
Soonrang <clearsbyul30@naver.com>
2024-09-09 22:32:17 +0900

    fix: 챌린지 리스트 반환 로직 수정

--- src/main/java/com/example/rewardservice/challenge/domain/repsoitory/ChallengeRepository.java
+++ src/main/java/com/example/rewardservice/challenge/domain/repsoitory/ChallengeRepository.java
@@ -12,9 +12,11 @@ import java.util.UUID;
 public interface ChallengeRepository extends JpaRepository<Challenge, UUID> {
     Page<Challenge> findByCategory(String category, Pageable pageable);
     //Page<Challenge> findByCategoryAndState(String category, String state, Pageable pageable);
-
-    Page<Challenge> findByCategoryAndState(String category,
-                                            String state,
-                                            Pageable pageable);
+    @Query("SELECT c FROM Challenge c " +
+            "WHERE c.category = :category " +
+            "AND c.state = :state")
+    Page<Challenge> findByCategoryAndState(@Param("category") String category,
+                                           @Param("state") String state,
+                                           Pageable pageable);
 }
 
