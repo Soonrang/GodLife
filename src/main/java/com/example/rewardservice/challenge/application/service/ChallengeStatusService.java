93c4dcb30d985926c87f08ab7f30321dab2c0869
Soonrang <clearsbyul30@naver.com>
2024-09-26 18:44:43 +0900

    refactor : 진행중, 진행전, 종료 공통클래스 생성

--- src/main/java/com/example/rewardservice/challenge/application/service/ChallengeStatusService.java
+++ src/main/java/com/example/rewardservice/challenge/application/service/ChallengeStatusService.java
@@ -8,15 +8,22 @@ import lombok.AllArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.beans.Transient;
 import java.time.LocalDate;
 import java.util.List;
+
+import static com.example.rewardservice.challenge.domain.vo.ChallengeStates.END_STATE;
 
 @Service
 @AllArgsConstructor
 public class ChallengeStatusService {
 
     private final ChallengeRepository challengeRepository;
+
+    private final static String UPCOMING_STATE = "진행전";
+    private final static String ONGOING_STATE = "진행중";
+    private final static String EDB_STATE = "종료";
+
+
 
     @PostConstruct
     public void init() {
@@ -31,19 +38,19 @@ public class ChallengeStatusService {
 
         List<Challenge> upcomingChallenges = challengeRepository.findChallengesForUpcoming(today);
         for (Challenge challenge : upcomingChallenges) {
-            challenge.updateStatus("진행전");
+            challenge.updateStatus(UPCOMING_STATE);
         }
 
         List<Challenge> ongoingChallenges = challengeRepository.findChallengesForOngoing(today);
         for (Challenge challenge : ongoingChallenges) {
-            challenge.updateStatus("진행중");
+            challenge.updateStatus(ONGOING_STATE);
         }
 
         List<Challenge> challengesToExpire = challengeRepository.findChallengesToExpire(today);
 
         // 상태를 '종료'로 변경
         for (Challenge challenge : challengesToExpire) {
-            challenge.updateStatus("종료");
+            challenge.updateStatus(END_STATE);
             challengeRepository.save(challenge);  // 상태 변경 후 저장
         }
 
---
+++ src/main/java/com/example/rewardservice/challenge/domain/vo/ChallengeStates.java
@@ -0,0 +1,9 @@
+package com.example.rewardservice.challenge.domain.vo;
+
+public class ChallengeStates {
+    public final static String UPCOMING_STATE = "진행전";
+    public final static String ONGOING_STATE = "진행중";
+    public final static String END_STATE = "종료";
+
+
+}
