c32f1eada11fd9531dac0438ebb4b58532e95065
Soonrang <clearsbyul30@naver.com>
2024-09-30 02:08:42 +0900

    refactor: 개설챌린지 목록 보여주는 순서 변경

--- src/main/java/com/example/rewardservice/challenge/domain/repsoitory/ChallengeAdminRepository.java
+++ src/main/java/com/example/rewardservice/challenge/domain/repsoitory/ChallengeAdminRepository.java
@@ -26,8 +26,19 @@ public interface ChallengeAdminRepository extends JpaRepository<Challenge, UUID>
                                                    @Param("state") String state,
                                                    Pageable pageable);
 
-    @Query("SELECT DISTINCT c FROM Challenge c WHERE c.user.email = :email AND c.isDeleted = false ")
+    @Query("SELECT DISTINCT c FROM Challenge c WHERE c.user.email = :email AND c.isDeleted = false " +
+            "ORDER BY " +
+            "(CASE " +
+            "   WHEN c.state = '종료' AND c.isClosed = false THEN 1 " +
+            "   WHEN c.state = '진행전' THEN 2 " +
+            "   WHEN c.state = '진행중' THEN 3 " +
+            "   WHEN c.state = '종료' AND c.isClosed = true THEN 4 " +
+            "   ELSE 5 " + 
+            " END)," +
+            "c.challengePeriod.startDate ASC")
     Page<Challenge> findAdminChallengesByAllStates(@Param("email") String email, Pageable pageable);
 
 
+
 }
+
